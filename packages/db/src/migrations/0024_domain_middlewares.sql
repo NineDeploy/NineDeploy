05fa7b61739d770bb575cf4fdc3335c2b30a9bff
Ersin KOÇ <ersinkoc@gmail.com>
2026-08-18 16:47:20 +0300

    fix(db): add missing statement-breakpoints in migrations and rebuild schema

--- packages/db/src/migrations/0024_domain_middlewares.sql
+++ packages/db/src/migrations/0024_domain_middlewares.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `domains` ADD `basic_auth` text;
-ALTER TABLE `domains` ADD `ip_allowlist` text;
-ALTER TABLE `domains` ADD `rate_limit_average` integer;
+ALTER TABLE `domains` ADD `basic_auth` text;--> statement-breakpoint
+ALTER TABLE `domains` ADD `ip_allowlist` text;--> statement-breakpoint
+ALTER TABLE `domains` ADD `rate_limit_average` integer;--> statement-breakpoint
 ALTER TABLE `domains` ADD `rate_limit_burst` integer;
--- packages/db/src/migrations/0025_extended_databases.sql
+++ packages/db/src/migrations/0025_extended_databases.sql
@@ -1,3 +1,3 @@
-ALTER TABLE `databases` ADD `web_gui_enabled` integer DEFAULT false NOT NULL;
-ALTER TABLE `databases` ADD `web_gui_port` integer;
+ALTER TABLE `databases` ADD `web_gui_enabled` integer DEFAULT false NOT NULL;--> statement-breakpoint
+ALTER TABLE `databases` ADD `web_gui_port` integer;--> statement-breakpoint
 ALTER TABLE `databases` ADD `extensions` text DEFAULT '[]' NOT NULL;
