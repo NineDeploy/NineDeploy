557161a9eee12e50fe61ee75cbfe13963f7cd281
Ersin KOÇ <ersinkoc@gmail.com>
2026-08-26 17:01:26 +0300

    chore(test): remove temporary mojibake repair and debug scripts

--- apps/server/test/builders/check2.mjs
+++
@@ -1,8 +0,0 @@
-import { readFileSync } from 'node:fs';
-const s = readFileSync('D:/Codebox/PROJECTS/NineDeploy/apps/server/test/builders/docker.test.ts', 'utf8');
-const matches = [];
-let i = -1;
-while ((i = s.indexOf("healthPath: '/health'", i + 1)) >= 0) {
-  matches.push({ idx: i, next: s.slice(i, i + 80) });
-}
-console.log(matches);
